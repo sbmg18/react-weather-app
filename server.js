64cbf61a1167a62b8e33b103572cee86863cc6e7
Shubham Gupta <sbmg18@gmail.com>
2017-05-15 23:38:30 +0530

    Server configuration for heroku

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var path = require('path');
 
+const PORT = process.env.PORT || 3000;
+
 var app = express();
 
+app.use(function(req, res, next) {
+
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect(`http://${req.hostname}${req.url}`);
+    }
+});
 app.use(express.static('public'));
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000, function() {
-    console.log("Express is up on port 3000.");
+app.listen(PORT, function() {
+    console.log(`Express is up on port ${PORT}.`);
 });
