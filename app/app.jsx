65d0b428413f8023c52eb9b238e8bf6adb4bee4f
Shubham Gupta <sbmg18@gmail.com>
2017-05-16 15:27:48 +0530

    Add foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,10 @@ import Weather from 'Weather';
 import About from 'About';
 import Examples from 'Examples';
 
+// Load foundation
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
     <Router>
         <div>
